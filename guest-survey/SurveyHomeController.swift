294459d176e3adabcb407d4896ba6ae02fe8085e
Donnie Armstrong <donaldjarmstrong@gmail.com>
2014-08-28 15:07:03 -0500

    renamed controller; added controllers folder to organize

--- guest-survey/SurveyHomeController.swift
+++ guest-survey/SurveyHomeController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SurveyHomeController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
