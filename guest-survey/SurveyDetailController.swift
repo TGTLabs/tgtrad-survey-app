b3f72a296fc7870f07736d6134eac3b37b4a39e6
Luke Gesior <luke.gesior@target.com>
2014-08-29 10:04:08 -0500

    small interface changes

---
+++ guest-survey/SurveyDetailController.swift
@@ -0,0 +1,16 @@
+//
+//  SurveyDetailController.swift
+//  guest-survey
+//
+//  Created by Luke.Gesior on 8/29/14.
+//  Copyright (c) 2014 Daniel.Chahla. All rights reserved.
+//
+
+import UIKit
+
+class SurveyDetailController: UIViewController {
+    
+    
+}
+
+
